b68acdb4172ee1b23374467c819d64a810edd1dc
altafziyaa <altafziyaa91@gmail.com>
2026-02-06 22:24:05 +0530

    Prepare product service for Vercel deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import authGatewayRoutes from "./src/routes/auth.gateway.Routes.js";
 import cartGatewayRoutes from "./src/routes/cart.gateway.route.js";
 import productGatewayRoutes from "./src/routes/product.gateway.route.js";
-import { verifyJwt } from "./src/middleware/authMiddleware.js";
+// import { verifyJwt } from "./src/middleware/authMiddleware.js";
 
 const app = express();
 
--- src/config/services.js
+++ src/config/services.js
@@ -1,5 +1,5 @@
 export default {
   AUTH_SERVICE: "https://service-auth.vercel.app/",
-  PRODUCT_SERVICE: "http://localhost:4002",
+  PRODUCT_SERVICE: "https://productservice-chi.vercel.app/",
   CART_SERVICE: "http://localhost:4003",
 };
