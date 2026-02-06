9875ea2755c07360412d1a42859d00c41ec8796e
altafziyaa <altafziyaa91@gmail.com>
2026-02-06 16:27:43 +0530

    Remove dotenv for Vercel runtime

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
 import gatewayRoutes from "./src/routes/auth.gateway.Routes.js";
-
-dotenv.config();
 
 const app = express();
 
