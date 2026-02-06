c472d3994d43a6d983999ce8d2a0046a3c7bdcd4
altafziyaa <altafziyaa91@gmail.com>
2026-02-06 20:51:42 +0530

    Update API Gateway with error handler

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 
-import gatewayRoutes from "./src/routes/auth.gateway.Routes.js";
+import authGatewayRoutes from "./src/routes/auth.gateway.Routes.js";
+import cartGatewayRoutes from "./src/routes/cart.gateway.route.js";
+import productGatewayRoutes from "./src/routes/product.gateway.route.js";
+import { verifyJwt } from "./src/middleware/authMiddleware.js";
 
 const app = express();
 
@@ -9,9 +12,20 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API Gateway Service is running 🚀");
+  res.send("API Gateway Service is running");
 });
 
-app.use("/api/auth", gatewayRoutes);
+app.use("/api/auth", authGatewayRoutes);
+app.use("/api/cart", verifyJwt, cartGatewayRoutes);
+app.use("/api/products", verifyJwt, productGatewayRoutes);
 
-export default app; // 🔥 Vercel requirement
+app.use((err, req, res, next) => {
+  console.error("Gateway Error:", err.message);
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
+
+export default app;
