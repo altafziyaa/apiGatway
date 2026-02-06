35c328d1900c9a437a3bcd83646475ba4964e510
altafziyaa <altafziyaa91@gmail.com>
2026-02-06 14:53:07 +0530

    'update

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from "express";
+import serverless from "serverless-http";
 import gatewayRoutes from "./src/routes/auth.gateway.Routes.js";
+
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Gateway Service is running 🚀");
+});
 
 app.get("/health", (req, res) => {
   res.json({ status: "API Gateway is running" });
 });
 
 app.use("/api/auth", gatewayRoutes);
-app.get("/", (req, res) => {
-  res.send("api gateway Service is running 🚀");
-});
+export default serverless(app);
