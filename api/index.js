bb75a8729bc414d6e94be682bfd953826770de4d
altafziyaa <altafziyaa91@gmail.com>
2026-02-06 15:56:55 +0530

    feat: api gateway vercel-ready setup

--- api/index.js
+++ api/index.js
@@ -3,17 +3,13 @@ import serverless from "serverless-http";
 import gatewayRoutes from "../src/routes/auth.gateway.Routes.js";
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API Gateway Service is running 🚀");
 });
 
-app.get("/health", (req, res) => {
-  res.json({ status: "API Gateway is running" });
-});
-
 app.use("/api/auth", gatewayRoutes);
 
 export default serverless(app);
+const PORT = 3000;
