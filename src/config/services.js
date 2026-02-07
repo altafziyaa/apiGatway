0e2e8d3cb7707d37e2119de0f3e8ed807cea7b7b
altafziyaa <altafziyaa91@gmail.com>
2026-02-07 19:17:31 +0530

    debugging

--- src/config/services.js
+++ src/config/services.js
@@ -1,5 +1,5 @@
 export default {
   AUTH_SERVICE: "https://service-auth.vercel.app/",
   PRODUCT_SERVICE: "https://productservice-chi.vercel.app/",
-  CART_SERVICE: "http://localhost:4003",
+  CART_SERVICE: "https://cartservice-nine.vercel.app/",
 };
